3961a831a5ef70cdf5ab00cd42f4a7947828f591
drab <58239644+pollocco@users.noreply.github.com>
2022-04-07 19:51:31 -0500

    Initial commit

---
+++ popup/tab_group_popup.js
@@ -0,0 +1,79 @@
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  browser.tabs.query({currentWindow: true, hidden: true}).then((tabList)=>{
+    for( tab of tabList ){
+        let tabItem = document.createElement( "div" );
+
+        let faviconDiv = document.createElement( "div" );
+
+        let faviconImg = document.createElement("img");
+        faviconImg.className = "tab_favicon";
+        faviconImg.src = tab.favIconUrl;
+
+        faviconDiv.appendChild(faviconImg);
+
+        tabItem.id = tab.id;
+        tabItem.className = "button tab";
+        tabItem.textContent += tab.title;
+
+        tabItem.appendChild(faviconDiv);
+
+        document.getElementById( "popup-content" ).appendChild(tabItem);
+      }
+  }
+
+  );
+
+  function onMoved(tab){
+    console.log("Moved " + tab.id);
+  }
+
+  document.addEventListener("click", (e) => {
+
+    function reportError(error) {
+      console.error(`Error: ${error}`);
+    }
+
+    if (e.target.classList.contains("tab")) {
+      browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then(()=>{
+            let tgt = e.target;
+            document.getElementById("popup-content")
+                .removeChild(document.getElementById(tgt.id));
+            browser.tabs.show(parseInt(tgt.id)).then(()=>{
+                browser.tabs.update(parseInt(tgt.id), {
+                    active: true
+                }).then(()=>{
+                      let moving = browser.tabs.move(parseInt(tgt.id), {index: -1});
+                      moving.then(onMoved, reportError);
+                })
+            })
+
+        })
+        .catch(reportError);
+    } else if (e.target.classList.contains("reset")) {
+      browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then(reset)
+        .catch(reportError);
+    }
+  });
+}
+
+let myPort = browser.runtime.connect({name:"hello"});
+myPort.postMessage({greeting: "hello from content script"});
+
+myPort.onMessage.addListener(function(m) {
+    console.log("In content script, received message from background script: ");
+    console.log(m.tabGroup);
+});
+  
+document.body.addEventListener("click", function() {
+    myPort.postMessage({greeting: "they clicked the page!"});
+});
+
+listenForClicks();
---
+++ tab_grouper.js
@@ -0,0 +1,84 @@
+browser.contextMenus.create({
+  id: "group-tab",
+  title: "Group tab",
+  contexts: ["tab"]
+})
+
+tab_group = [];
+tab_group_id = -1;
+
+browser.contextMenus.onClicked.addListener(function(info, tab){
+  switch(info.menuItemId){
+    case "group-tab":
+      console.log("Clicked");
+      addToTabGroup(tab);
+  }
+})
+
+function onHidden() {
+  console.log(`Hidden`);
+}
+
+function onShown() {
+  console.log(`Shown`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onCreated(tab){
+  tab_group_id = tab.id;
+}
+
+function addToTabGroup(tab){
+/*   if( tab_group.length == 0 ){
+    let newTabCreate = browser.tabs.create({
+      active: false,
+      index: 0,
+      pinned: true
+    })
+    newTabCreate.then( onCreated, onError );
+    browser.tabs.onActivated.addListener( tabGroupContextMenu );
+  } */
+  tab_group.push(tab.id);
+  browser.tabs.hide(tab.id).then(onHidden, onError);
+  browser.tabs.query({
+    currentWindow: true
+  }).then((tabs)=>{
+/*     for( tab of tabs ){
+      browser.runtime.sendMessage(tab.id, {
+        command: "tab_group_update",
+        tabGroup: tab_group
+      }).catch(onError);
+    } */
+  })
+
+}
+
+let portFromCS;
+
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({greeting: "hi there content script!"});
+  portFromCS.onMessage.addListener(function(m) {
+    portFromCS.postMessage({tabGroup: m.tab_group});
+  });
+}
+
+browser.runtime.onConnect.addListener(connected);
+
+browser.browserAction.onClicked.addListener(function() {
+  portFromCS.postMessage({greeting: "they clicked the button!"});
+});
+
+try {
+  console.log("browser : ");
+  console.log(browser);
+  console.log("browser.tabs : ");
+  console.log(browser.tabs);
+
+
+} catch(err) {
+  console.log("err : ", err);
+}
